9ff7d293b3ef7aa9be68fa0c91f66f319c293fe6
Edgar Lorenz G. Castro <elgc@Edgars-iMac.local>
2025-02-15 18:39:23 +0800

    swipe test

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Header from "./header"
 import Content from "./content/content"
+import { useSwipeable } from "react-swipeable";
 
 export default function Editor({focus, scroll, switchView}) {
   const [editMode, setEditMode] = useState('availability');
@@ -9,8 +10,16 @@ export default function Editor({focus, scroll, switchView}) {
     setEditMode(editMode === 'availability' ? 'time schedule' : 'availability')
   }
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => scroll('right'),
+    onSwipedRight: () => scroll('left'),
+    swipeDuration: 300,
+    preventScrollOnSwipe: true,
+    trackMouse: true
+  });
+
   return (
-    <div id={"editor"}>
+    <div {...handlers} id={"editor"}>
       <Header focus={focus} scroll={scroll} switchView={switchView}/>
       <Content focus={focus} editMode={editMode} modeSwitch={modeSwitch} />
     </div>
